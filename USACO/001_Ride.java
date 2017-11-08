c77f2517a4b5594d5f55da8c21107661ec0a4415
Aaron Covington <TheAaronCovington@gmail.com>
2017-11-08 15:51:05 -0600

    Update 001_Ride.java

--- USACO/001_Ride.java
+++ USACO/001_Ride.java
@@ -3,32 +3,36 @@ ID: xervean1
 LANG: JAVA
 TASK: ride
 */
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class ride {
-	public static void main (String [] args) throws IOException {
-	    BufferedReader input = new BufferedReader(new FileReader("ride.in"));
-	    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
-	    String values = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	    String comet = input.readLine();
-	    String group = input.readLine();
-	    int sum1 = 1; int sum2 = 1;
-	    for(int i = 0; i < comet.length(); i++){
-	    	if(values.indexOf(comet.charAt(i)) != -1){
-	    		sum1 *= values.indexOf(comet.charAt(i));
-	    	}
-	    }
-	    for(int i = 0; i < group.length(); i++){
-	    	if(values.indexOf(group.charAt(i)) != -1){
-	    		sum2 *= values.indexOf(group.charAt(i));
-	    	}
-	    }
-	    if(sum1 % 47 != sum2 % 47){
-	    	out.println("STAY");
-	    }
-	    else{
-	    	out.println("GO");
-	    }
-	    out.close();
-	  }
+  public static void main(String[] args) throws IOException {
+    Scanner input = new Scanner(new BufferedReader(new FileReader("ride.in")));
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+    String comet = input.nextLine();
+    int prod1 = 1, prod2 = 1;
+    String group = input.nextLine();
+    for(int i = 0; i < comet.length(); i++) {
+      int temp = (comet.charAt(i) - 'A') + 1;
+      prod1 *= temp;
+    }
+    for(int i = 0; i < group.length(); i++) {
+      int temp = (group.charAt(i) - 'A') + 1;
+      prod2 *= temp;
+    }
+    if(prod1 % 47 == prod2 % 47) {
+      out.println("GO");
+    }
+    else {
+      out.println("STAY");
+    }
+    out.close();
+  }
 }
